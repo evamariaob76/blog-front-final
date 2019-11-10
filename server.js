80e24a3ebd562b221d8abbe8d65eebeac45528f3
mortiz <miguelaob@gmail.com>
2019-11-10 21:34:22 +0100

    Herok

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist'));
+app.use(express.static(__dirname+'/dist/blog-front-final'));
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/blog-front-final/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
