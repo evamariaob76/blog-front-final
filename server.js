9fddc40022c563326b8d96c92f52f48dfcf26ae4
mortiz <miguelaob@gmail.com>
2019-11-10 21:55:17 +0100

    Herok

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/build'));
+app.use(express.static(__dirname+'/dist/my-app'));
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/build/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/my-app/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
